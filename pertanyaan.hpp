13223121fbb37c269807e07dc54fd56db32c4fad
Ahmad <ahmadhawari@students.amikom.ac.id>
2022-06-28 03:30:01 +0700

    add fun question

--- pertanyaan.hpp
+++ pertanyaan.hpp
@@ -12,25 +12,33 @@ void gameover(){
 void masukGame(){
     std::string pertanyaan[5];
 
-    pertanyaan[0] = "Volvo";
-    pertanyaan[1] = "BMW";
-    pertanyaan[2] = "Volvo";
-    pertanyaan[3] = "BMW";
-    pertanyaan[4] = "Volvo";
+    pertanyaan[0] = "Negara mana yang hanya terdapat dua wanita saja?";
+    pertanyaan[1] = "Ada ayam lima, dikali dua. Berapa total ayam semuanya? ";
+    pertanyaan[2] = "Jika dibutuhkan waktu 10 menit untuk merebus 1 butir telur, Berapa waktu yang diperlukan untuk merebus 5 butir telur? ";
+    pertanyaan[3] = "Tebak binatang apa yang jago renang?";
+    pertanyaan[4] = "Saat di balik akan berkurang 3. Apakah itu? ";
 
     std::string jawaban1[5]; //jawaban1
-    jawaban1[0] ="";
-    jawaban1[1] ="";
-    jawaban1[2] ="";
-    jawaban1[3] ="";
-    jawaban1[4] ="";
+    jawaban1[0] ="A. ITALIA";
+    jawaban1[1] ="A. 10";
+    jawaban1[2] ="A. 50 Menit";
+    jawaban1[3] ="A. Bebek";
+    jawaban1[4] ="A. Angka 12";
 
     std::string jawaban2[5]; //jawaban2
-    jawaban2[0] ="";
-    jawaban2[1] ="";
-    jawaban2[2] ="";
-    jawaban2[3] ="";
-    jawaban2[4] ="";
+    jawaban2[0] ="B. JERMAN";
+    jawaban2[1] ="B. 5";
+    jawaban2[2] ="B. 10 Menit";
+    jawaban2[3] ="B. Ikan";
+    jawaban2[4] ="B. Angka 9";
+
+    std::string benar[5]; //benar
+    benar[0] ="ITALIA (ITA dan LIA)";
+    benar[1] ="5, Karena yang di kali 2, yang di darat 3 jadi total semuanya ada 5";
+    benar[2] ="Tetap 10 menit, Tinggal rebus aja secara bersama-sama";
+    benar[3] ="Bebek. Kalau ikan bukan renang, tapi menyelam";
+    benar[4] ="Angka 9. karena angka 9 kalo di balik jadi angka 6.";
+
 
 //jawabannya dibuat 2 karena biar bisa dispawn secara berdampingan
     char kunci[5]={'A','B','B','A','B'};//kunci jawaban
